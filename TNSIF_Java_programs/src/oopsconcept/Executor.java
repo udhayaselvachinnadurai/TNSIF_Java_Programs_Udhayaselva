43bffd42b416c056bf0b4ebffe60e3dfe3ee1c31
Udhayaselva <Udhayaselva@LAPTOP-LS9TLLK1>
2025-07-01 09:46:20 +0530

    initial commit

---
+++ TNSIF_Java_programs/src/oopsconcept/Executor.java
@@ -0,0 +1,16 @@
+package oopsconcept;
+public class Executor {
+
+	public static void main(String[] args) {
+		
+		EncapsulationDemo ed = new EncapsulationDemo();
+		ed.setName("MS Dhoni");
+		ed.setAge(43);
+		ed.setJersyNo(7);
+		System.out.println(ed);
+		
+		
+
+	}
+
+}
