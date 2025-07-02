57d6e8f1d800da3248d6a082bb7528f6111a796a
Udhayaselva <Udhayaselva@LAPTOP-LS9TLLK1>
2025-07-02 13:47:57 +0530

    commit new

---
+++ TNSIF_Java_programs/src/packagedemo/PackageMain.java
@@ -0,0 +1,18 @@
+package packagedemo;
+
+		import packageandaccessmodifiers.PackageExample;
+
+		public class PackageMain {
+
+			public static void main(String[] args) {
+
+				PackageExample pe = new PackageExample();
+				pe.display();
+
+			}
+
+		}
+
+	
+
+
