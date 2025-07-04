b4c217405cae1584f302e06348d29745bf18cd1d
Udhayaselva <Udhayaselva@LAPTOP-LS9TLLK1>
2025-07-04 13:59:18 +0530

    Initial Commit3

---
+++ TNSIF_Java_programs/src/nonaccessmodifiers/FinalDemo.java
@@ -0,0 +1,21 @@
+package nonaccessmodifiers;
+
+final class FinalDemo {
+	
+	final int Max = 100;
+	
+	 void display() {
+		System.out.println("show the value of max: "+Max);
+	}
+
+	public static void main(String[] args) {
+		FinalDemo fd = new FinalDemo();
+		fd.display();
+		
+
+	}
+
+}
+
+
+
