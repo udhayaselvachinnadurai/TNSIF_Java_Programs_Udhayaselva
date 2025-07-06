218906515cf5eb1cb9ce4347d52ac2bd9abfe01b
Udhayaselva <Udhayaselva@LAPTOP-LS9TLLK1>
2025-07-06 13:08:42 +0530

    Initial Commit

---
+++ TNSIF_Java_programs/src/exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,11 @@
+package exceptionhandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
+
